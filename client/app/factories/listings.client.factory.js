6fb4ee97a2814262b3c705a1f7c95593abc46e33
Xiaoming Lu <xiaoming@ufl.edu>
2017-09-13 14:16:18 -0400

    change the routes for assignment6

--- client/app/factories/listings.client.factory.js
+++ client/app/factories/listings.client.factory.js
@@ -2,23 +2,23 @@ angular.module('listings').factory('Listings', ['$http',
   function($http) {
     var methods = {
       getAll: function() {
-        return $http.get('http://localhost:8080/api/listings');
+        return $http.get('https://still-thicket-29876.herokuapp.com/api/listings');
       },
 
       create: function(listing) {
-        return $http.post('http://localhost:8080/api/listings', listing);
+        return $http.post('https://still-thicket-29876.herokuapp.com/api/listings', listing);
       }, 
 
       read: function(id) {
-        return $http.get('http://localhost:8080/api/listings/' + id);
+        return $http.get('https://still-thicket-29876.herokuapp.com/api/listings/' + id);
       }, 
 
       update: function(id, listing) {
-        return $http.put('http://localhost:8080/api/listings/' + id, listing);
+        return $http.put('https://still-thicket-29876.herokuapp.com/api/listings/' + id, listing);
       }, 
 
       delete: function(id) {
-        return $http.delete('http://localhost:8080/api/listings/' + id);
+        return $http.delete('https://still-thicket-29876.herokuapp.com/api/listings/' + id);
       }
     };
 
