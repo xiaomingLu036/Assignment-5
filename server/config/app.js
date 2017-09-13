8e3a9819acdf37fd1e822d22ceee6ac152b4a8bc
Xiaoming Lu <xiaoming@ufl.edu>
2017-09-13 12:20:16 -0400

    This change is for Assignment 6, so it would read configuration from heroku instead of config file.

--- server/config/app.js
+++ server/config/app.js
@@ -1,10 +1,9 @@
-var config = require('./config'), 
-    mongoose = require('mongoose'),   
+var mongoose = require('mongoose'),   
     express = require('./express');
 
 module.exports.start = function() {
   var app = express.init();
-  app.listen(config.port, function() {
-    console.log('App listening on port', config.port);
+  app.listen(process.env.port, function() {
+    console.log('App listening on port', process.env.port);
   });
 };
