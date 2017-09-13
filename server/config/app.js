689e16f50d31b98aa810a40ea33c568567ecd63f
Xiaoming Lu <xiaoming@ufl.edu>
2017-09-13 12:50:48 -0400

    Assignment 6 setting the port from heroku

--- server/config/app.js
+++ server/config/app.js
@@ -3,7 +3,8 @@ var mongoose = require('mongoose'),
 
 module.exports.start = function() {
   var app = express.init();
+  var port =  process.env.PORT || 8080;
   app.listen(process.env.port, function() {
-    console.log('App listening on port', process.env.port);
+    console.log('App listening on port', port);
   });
 };
